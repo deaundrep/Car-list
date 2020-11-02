fe74f674b12fb9460d10f39d263b30b73cd35a2d
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-11-02 09:04:36 -0500

    done

--- car-list/app.js
+++ car-list/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -32,10 +33,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+const methodOverride = require = ('method-ride')
+app.use(methodOverride('_method'))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+const carRouter = require('./routes/Cars/carRoutes.js')
+app.use('/cars', carRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
